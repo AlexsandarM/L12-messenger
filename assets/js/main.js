04bcd2740a38d4e76ff740eef466a7b2434fc8c4
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2020-07-13 15:45:03 +0200

    edit 02

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,65 @@
+// Edit 01
 
-let userMsg = document.getElementById('message');
-let deliveredmsg = document.getElementById('deliveredmsg');
+// let userMsg = document.getElementById('message');
+// let deliveredmsg = document.getElementById('deliveredmsg');
+// let btn = document.querySelector('.btn');
+
+
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+
+//   let msgValue = userMsg.value.trim().toUpperCase();
+  
+//   if(msgValue === '') {
+//     let showMsg = `<span class="error">Please enter valid message!</span>`;
+//     deliveredmsg.innerHTML = showMsg;
+//   } else {
+//     let showMsg = `<span class="success">Last Message Delivered</span><br><strong>${msgValue}</strong>`;
+//     deliveredmsg.innerHTML = showMsg;
+//   };
+
+//   userMsg.value = '';
+
+// })
+
+
+// Edit 02
+
+let userInput = document.getElementById('input');
+let outputmsg = document.getElementById('outputmsg');
 let btn = document.querySelector('.btn');
+let form = document.getElementById('form');
+let small = document.querySelector('small');
+// console.log(small)
 
+// Functions 
 
-btn.addEventListener('click', (e) => {
+function showError(message) {
+
+  small.setAttribute('class', 'text-center my-2 error')
+  small.innerText = message;
+  outputmsg.innerHTML = '';
+}
+
+function showSuccess(message) {
+
+  small.setAttribute('class', 'text-center my-2 success')
+  small.innerText = message;
+}
+
+// Listeners
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  let msgValue = userMsg.value.trim().toUpperCase();
-  
-  if(msgValue === '') {
-    let showMsg = `<span class="error">Please enter valid message!</span>`;
-    deliveredmsg.innerHTML = showMsg;
+  let userMsg = userInput.value.trim().toUpperCase();
+
+  if (userMsg === '') {
+    showError('Please enter valid message!');
   } else {
-    let showMsg = `<span class="success">Last Message Delivered</span><br><strong>${msgValue}</strong>`;
-    deliveredmsg.innerHTML = showMsg;
+    showSuccess('Last message was delivered');
+    outputmsg.innerHTML = userMsg;
   };
 
-  userMsg.value = '';
-
+  userInput.value = '';
 })
